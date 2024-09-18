4dc2953af5dc4b37d2cff4e5254bf863d950bda6
codatr0n <inboxtoerik@gmail.com>
2024-09-18 22:55:52 +0200

    splitting config

--- configuration.nix
+++ configuration.nix
@@ -5,8 +5,22 @@
 { config, pkgs, ... }:
 
 {
+
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+  };
+
+
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      ./keyboard.nix
+      ./sound.nix
+      ./packages.nix
+      ./fonts.nix
+      ./zsh.nix
+      ./gnome.nix
+
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
@@ -28,43 +42,6 @@
   # Set your time zone.
   time.timeZone = "Europe/Copenhagen";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_DK.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "da_DK.UTF-8";
-    LC_IDENTIFICATION = "da_DK.UTF-8";
-    LC_MEASUREMENT = "da_DK.UTF-8";
-    LC_MONETARY = "da_DK.UTF-8";
-    LC_NAME = "da_DK.UTF-8";
-    LC_NUMERIC = "da_DK.UTF-8";
-    LC_PAPER = "da_DK.UTF-8";
-    LC_TELEPHONE = "da_DK.UTF-8";
-    LC_TIME = "da_DK.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
-  # services.gnome.core-utilities.enable = false; # disable built-in gnome apps
-  # exclude individual gnome apps
-  environment.gnome.excludePackages = with pkgs.gnome; [
-    epiphany
-    gnome-calendar
-    gnome-maps
-    gnome-music
-    geary
-    totem
-    yelp
-  ];
- 
-
-
-
   # Enable auto optimising the store - funk 2024.09.18
   nix.settings.auto-optimise-store = true;
 
@@ -80,49 +57,25 @@
     sudo.wheelNeedsPassword = false;
   };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us,dk";
-    variant = "";
-  };
 
-  # Configure console keymap
-  # console.keyMap = "dk-latin1";
-  console.keyMap = "us";
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # add zsh shell
+    # add zsh shell
   environment.shells = with pkgs; [ zsh ];
   programs.zsh = {
-    enable = true; 
+    enable = true;
     enableCompletion = true;
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
 
     shellAliases = {
       "ls" = "eza -alh --group-directories-first --time-style=long-iso --icons --git";
+      "nrs" = "sudo nixos-rebuild switch";
     };
   };
 
@@ -154,35 +107,6 @@
   };
 
 
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-
-      firefox
-      google-chrome
-
-      curl
-      wget
-      fwupd
-      git
-      python311 
-      gh
-
-      # printing
-      gutenprint
-      hplip
-
-      kdePackages.kate
-
-      alacritty
-      eza
-      bat
-
-
-  ];
-
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -212,12 +136,7 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.05";
+
 
 }
