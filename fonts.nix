3fa7051a1497df2524f557df1f559b396670b756
codatr0n <inboxtoerik@gmail.com>
2024-10-03 11:37:43 +0200

    add fonts

--- fonts.nix
+++ fonts.nix
@@ -1,22 +1,36 @@
 { config, pkgs, ... }:
 {
 
+  fonts.fontconfig.enable = true;
+
   fonts.packages = with pkgs; [
     noto-fonts
     ubuntu_font_family
     noto-fonts-emoji
-    liberation_ttf
+    liberation_ttf # replacements for Times New Roman, Arial, and Courier New
+    corefonts # microsoft fonts
+    gelasio # font which is metric-compatible with Microsoft's Georgia
+
+    # developers fonts
+    dina-font
     fira
     fira-code
     fira-code-symbols
     fira-code-nerdfont
-    mplus-outline-fonts.githubRelease
-    dina-font
     jetbrains-mono
+    julia-mono
 
     (nerdfonts.override { fonts = [
-      "JetBrainsMono"
-    ];}
+          "JetBrainsMono"
+          "Iosevka"
+          "Hack"
+          "Meslo"
+          "Terminus"
+          "Monoid"
+          "Inconsolata"
+
+        ];
+      }
     )
   ];
 
