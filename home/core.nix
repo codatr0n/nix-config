ddfe8d0baf24e8921ec4dfb0bf11c58253989df0
Funk <inboxtoerik@gmail.com>
2025-01-23 20:38:46 +0100

    implement stateVersion as parameter

--- home/core.nix
+++ home/core.nix
@@ -1,4 +1,4 @@
-{username, ...}: {
+{username, stateVersion, ...}: {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home = {
@@ -13,7 +13,7 @@
     # You can update Home Manager without changing this value. See
     # the Home Manager release notes for a list of state version
     # changes in each release.
-    stateVersion = "24.05";
+    stateVersion = "${stateVersion}";
   };
 
   # Let Home Manager install and manage itself.
