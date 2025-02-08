ca1567e0d982f8e043daf69596a98d74bce2fff7
ErikMK <mail@erikmk.dk>
2025-02-08 15:53:47 +0100

    reorg fingerprint

---
+++ fingerprint.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+
+{
+  # Fingerprint reader
+
+  # Enable fingerprint reader
+  services.fprintd.enable = true;
+  services.fprintd.tod.enable = true;
+  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090;
+
+}
