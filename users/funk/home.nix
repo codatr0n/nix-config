ddd3c4429d8cb310f80dd51c5ab2595ba2f310dd
ErikMK <mail@erikmk.dk>
2025-03-22 00:18:31 +0100

    remove warp from home manager

--- users/funk/home.nix
+++ users/funk/home.nix
@@ -10,7 +10,6 @@
     ../../home/programs/common.nix
     ../../home/programs/zsh.nix
     ../../home/programs/alacritty.nix
-    ../../home/programs/warp.nix
     ../../home/programs/starship.nix
     ../../home/programs/dropbox.nix
     ../../home/config/fonts.nix
