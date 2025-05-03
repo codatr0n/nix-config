3cab17ac4d72ce96ece6238f5a8403507e7d9949
ErikMK <mail@erikmk.dk>
2025-05-03 21:20:03 +0200

    fix gnome settings, enable partition manager

--- modules/kde.nix
+++ modules/kde.nix
@@ -10,7 +10,9 @@
   services.desktopManager.plasma6.enable = true;
 
 
-  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+  services.udev.packages = with pkgs; [
+    gnome-settings-daemon
+  ];
 
   # exclude individual kde pacakges/apps
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
@@ -20,5 +22,7 @@
   ];
 
   programs.dconf.enable = true;
+
+  programs.partition-manager.enable = true; # KDE Partition Manager
 
 }
